75ce617467428d22e84974986ccf57b3cfb3c0f2
abdelazeem-5 <154232833+abdelazeem-5@users.noreply.github.com>
2024-04-08 00:49:51 +0200

    GPA

--- x4.cpp
+++ x4.cpp
@@ -1,21 +1,26 @@
 
 #include <iostream>
-#include <string>
 using namespace std;
 
-int main() {
-    string word;
-    cout << "Enter a word: ";
-    for (char & c : word)
-     {
+int main() 
+{
+float GPA;
+int Financial_condition;
 
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') 
-        {
-            c = '!';
-        }
-    }
+cout <<"Enter the GPA\n";
+cin >> GPA;
 
-    cout <<  word ;
+cout <<"Enter the Fainancial need\n";
+cin >> Financial_condition;
+
+if (GPA >= 3.5 && Financial_condition >= 50000)
+{
+cout << "The student is eligible\n";
+}
+else
+{
+cout << "The student is not eligible\n";
+}
 
     return 0;
 }
